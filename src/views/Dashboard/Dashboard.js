5608460f08cbfdac8dfea3d28381b1a6e6557177
DoguD <dogudeniz.ugur@gmail.com>
2022-01-13 00:47:15 +0300

    what is easyblock text

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -231,6 +231,61 @@ export default function Dashboard() {
             </Portal>
             <Flex flexDirection="column" pt={{base: "120px", md: "75px"}} maxWidth={"1400px"} paddingLeft={0}
                   paddingRight={0}>
+                <Flex
+                    bg="#FFFFFF"
+                    align="start"
+                    justify="center"
+                    borderRadius="15px"
+                    flexDirection={"column"}
+                    padding={4}
+                    marginBottom={4}
+                >
+                    <Text style={{
+                        marginBottom: 16,
+                        fontWeight: "bold",
+                        fontSize: 20,
+                        color: "#3e68a4",
+                        marginTop: 8
+                    }}>What is EasyBlock? <br/>
+                        <span
+                            style={{fontWeight: 'normal', color: "#000000", fontSize: 16}}>
+                            EasyBlock is a protocol running on Fantom Network which enables investors of all sizes to invest in StrongBlock Nodes. StrongBlock requires a minimum investment
+                            amount of <b>{(strongPrice * 10).toFixed(0)} $</b> and runs on Ethereum Mainnet which has really high gas-fees. These conditions make StrongBlock unaccesible for most of investors.
+                            On contrary, <b>EasyBlock</b> has a minimum investment amount of <b>10 $</b> per share and minimal gas fees. <br/>
+                            Amount generated from share sales are bridged to Ethereum Mainnet and used to purchase StrongBlock Nodes. Revenue generated from those nodes are bridged back to Fantom on optimal intervals and distributed to shareholders.
+                        </span></Text>
+                    <Button
+                        p="0px"
+                        variant="no-hover"
+                        bg="transparent"
+                        my={{sm: "1.5rem", lg: "0px"}}
+                        onClick={() => window.open("https://docs.easyblock.finance", '_blank')}
+                    >
+                        <Text
+                            fontSize="sm"
+                            color={"#3e68a4"}
+                            fontWeight="bold"
+                            cursor="pointer"
+                            transition="all .5s ease"
+                            my={{sm: "1.5rem", lg: "0px"}}
+                            _hover={{me: "4px"}}
+                        >
+                            Learn More
+                        </Text>
+                        <Icon
+                            color={"#3e68a4"}
+                            as={BsArrowRight}
+                            w="20px"
+                            h="20px"
+                            fontSize="2xl"
+                            transition="all .5s ease"
+                            mx=".3rem"
+                            cursor="pointer"
+                            pt="4px"
+                            _hover={{transform: "translateX(20%)"}}
+                        />
+                    </Button>
+                </Flex>
                 <SimpleGrid columns={{sm: 1, md: 2, xl: 4}} spacing="24px" paddingLeft={0} paddingRight={0}>
                     <Card minH="83px">
                         <CardBody>
