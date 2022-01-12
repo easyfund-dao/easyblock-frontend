199a36c8559433f2989d602818af3764423ce328
DoguD <dogudeniz.ugur@gmail.com>
2022-01-13 01:11:56 +0300

    New contract address and new ABI

--- src/contracts/EasyBlock.js
+++ src/contracts/EasyBlock.js
@@ -1,199 +1,217 @@
-export const CONTRACT_ADDRESS = "0xe5221490775D820F767d2903b708562d8cC7D6af";
-export const EASYBLOCK_ABI =  [{
-        "inputs": [{"internalType": "uint256", "name": "_fee", "type": "uint256"}],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-    }, {
-        "anonymous": false,
-        "inputs": [{
-            "indexed": false,
-            "internalType": "uint256",
-            "name": "shareAmount",
-            "type": "uint256"
-        }, {
-            "indexed": false,
-            "internalType": "uint256",
-            "name": "investmentInUSD",
-            "type": "uint256"
-        }, {"indexed": false, "internalType": "address", "name": "shareHolder", "type": "address"}],
-        "name": "Investment",
-        "type": "event"
-    }, {
-        "anonymous": false,
-        "inputs": [{
-            "indexed": false,
-            "internalType": "uint256",
-            "name": "amount",
-            "type": "uint256"
-        }, {"indexed": false, "internalType": "address", "name": "shareHolder", "type": "address"}],
-        "name": "RewardCollected",
-        "type": "event"
-    }, {
-        "inputs": [{"internalType": "address", "name": "_tokenAddress", "type": "address"}, {
-            "internalType": "uint256",
-            "name": "_tokenPrice",
-            "type": "uint256"
-        }], "name": "addPurchaseToken", "outputs": [], "stateMutability": "nonpayable", "type": "function"
-    }, {
-        "inputs": [{"internalType": "address", "name": "_token", "type": "address"}, {
-            "internalType": "uint256",
-            "name": "_shareCount",
-            "type": "uint256"
-        }], "name": "buyShares", "outputs": [], "stateMutability": "nonpayable", "type": "function"
-    }, {
-        "inputs": [],
-        "name": "claimRewards",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "name": "claimableReward",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "uint256", "name": "_amount", "type": "uint256"}],
-        "name": "depositRewards",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "address", "name": "_tokenAddress", "type": "address"}, {
-            "internalType": "uint256",
-            "name": "_tokenPrice",
-            "type": "uint256"
-        }], "name": "editPurchaseToken", "outputs": [], "stateMutability": "nonpayable", "type": "function"
-    }, {
-        "inputs": [],
-        "name": "fee",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [],
-        "name": "feeCollector",
-        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "name": "holders",
-        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [],
-        "name": "manager",
-        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "name": "nodeHolders",
-        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "name": "purchaseTokens",
-        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "name": "purchaseTokensPrice",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [],
-        "name": "rewardToken",
-        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "address", "name": "_tokenAddress", "type": "address"}],
-        "name": "setDepositToken",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "uint256", "name": "_fee", "type": "uint256"}],
-        "name": "setFee",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "address", "name": "_address", "type": "address"}],
-        "name": "setFeeCollector",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "address", "name": "_address", "type": "address"}],
-        "name": "setManager",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "address", "name": "_address", "type": "address"}],
-        "name": "setNodeHolder",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "name": "shareCount",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [],
-        "name": "sharePurchaseEnabled",
-        "outputs": [{"internalType": "bool", "name": "", "type": "bool"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [],
-        "name": "toggleSharePurchaseEnabled",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }, {
-        "inputs": [],
-        "name": "totalInvestmentsInUSD",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [],
-        "name": "totalRewardsDistributedInUSD",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [],
-        "name": "totalShareCount",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "name": "totalUserRewards",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function"
-    }, {
-        "inputs": [{"internalType": "address", "name": "_targetAddress", "type": "address"}, {
-            "internalType": "uint256",
-            "name": "_shareAmount",
-            "type": "uint256"
-        }], "name": "transferSharesFromManager", "outputs": [], "stateMutability": "nonpayable", "type": "function"
-    }, {
-        "inputs": [{"internalType": "address", "name": "_token", "type": "address"}, {
-            "internalType": "uint256",
-            "name": "_amount",
-            "type": "uint256"
-        }], "name": "withdrawToManager", "outputs": [], "stateMutability": "nonpayable", "type": "function"
-    }];
+export const CONTRACT_ADDRESS = "0x17c015BDD4136bE59dC07b1E046259864D1b2DFB";
+export const EASYBLOCK_ABI = [{
+    "inputs": [{"internalType": "uint256", "name": "_fee", "type": "uint256"}],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+}, {
+    "anonymous": false,
+    "inputs": [{
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "shareAmount",
+        "type": "uint256"
+    }, {"indexed": false, "internalType": "uint256", "name": "investmentInUSD", "type": "uint256"}, {
+        "indexed": false,
+        "internalType": "address",
+        "name": "shareHolder",
+        "type": "address"
+    }],
+    "name": "Investment",
+    "type": "event"
+}, {
+    "anonymous": false,
+    "inputs": [{"indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256"}, {
+        "indexed": false,
+        "internalType": "address",
+        "name": "shareHolder",
+        "type": "address"
+    }],
+    "name": "RewardCollected",
+    "type": "event"
+}, {
+    "inputs": [{"internalType": "address", "name": "_tokenAddress", "type": "address"}, {
+        "internalType": "uint256",
+        "name": "_tokenPrice",
+        "type": "uint256"
+    }], "name": "addPurchaseToken", "outputs": [], "stateMutability": "nonpayable", "type": "function"
+}, {
+    "inputs": [{"internalType": "address", "name": "_token", "type": "address"}, {
+        "internalType": "uint256",
+        "name": "_shareCount",
+        "type": "uint256"
+    }], "name": "buyShares", "outputs": [], "stateMutability": "nonpayable", "type": "function"
+}, {
+    "inputs": [],
+    "name": "claimFees",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "claimRewards",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "name": "claimableReward",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "uint256", "name": "_amount", "type": "uint256"}],
+    "name": "depositRewards",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "address", "name": "_tokenAddress", "type": "address"}, {
+        "internalType": "uint256",
+        "name": "_tokenPrice",
+        "type": "uint256"
+    }], "name": "editPurchaseToken", "outputs": [], "stateMutability": "nonpayable", "type": "function"
+}, {
+    "inputs": [],
+    "name": "fee",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "feeCollector",
+    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "name": "holders",
+    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "manager",
+    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "name": "newInvestments",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "name": "nodeHolders",
+    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "name": "purchaseTokens",
+    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "name": "purchaseTokensPrice",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "rewardToken",
+    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "address", "name": "_tokenAddress", "type": "address"}],
+    "name": "setDepositToken",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "uint256", "name": "_fee", "type": "uint256"}],
+    "name": "setFee",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "address", "name": "_address", "type": "address"}],
+    "name": "setFeeCollector",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "address", "name": "_address", "type": "address"}],
+    "name": "setManager",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "uint256", "name": "_count", "type": "uint256"}],
+    "name": "setNodeCount",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "address", "name": "_address", "type": "address"}],
+    "name": "setNodeHolder",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "name": "shareCount",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "sharePurchaseEnabled",
+    "outputs": [{"internalType": "bool", "name": "", "type": "bool"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "toggleSharePurchaseEnabled",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "totalInvestmentsInUSD",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "totalRewardsDistributedInUSD",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "totalShareCount",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "name": "totalUserRewards",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [{"internalType": "address", "name": "_targetAddress", "type": "address"}, {
+        "internalType": "uint256",
+        "name": "_shareAmount",
+        "type": "uint256"
+    }], "name": "transferSharesFromManager", "outputs": [], "stateMutability": "nonpayable", "type": "function"
+}, {
+    "inputs": [{"internalType": "address", "name": "_token", "type": "address"}, {
+        "internalType": "uint256",
+        "name": "_amount",
+        "type": "uint256"
+    }], "name": "withdrawToManager", "outputs": [], "stateMutability": "nonpayable", "type": "function"
+}];
